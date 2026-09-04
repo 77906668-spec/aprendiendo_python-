12a94a8a9f6220893cb27c7ded2bb53d3e2cbb47
chelo <77906668@institutourusayhua.edu.pe>
2026-09-04 12:33:00 -0500

    Add files via upload

---
+++ Nueva carpeta/hola mundo.py
@@ -0,0 +1,28 @@
+# operador binario
+print(5 + 4)
+print(-5 + 4.25)
+# operador unario
+print(-4)
+print(-4.96)
+print(+4)
+# Operadores y sus prioridades
+print("operadores con prioridad", 2 + 3 *5)
+# operadores y sus enlaces
+print("resultado de operador con enlaces", 9 % 6 % 2)
+print("resultado de operador con enlaces", 2 ** 2 ** 3)
+# Operadores y paréntesis
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+# Nombres de Variables
+var = 1
+print(var)
+variable = 589
+cuenta_balance = 1000.0
+nombre_cliente = 'chelito'
+print(variable, cuenta_balance, nombre_cliente)
+print(variable)
+var = "3.8.5"
+print("python version: " , var)
+
+
+
+
