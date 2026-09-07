3404a2788be2724c3cbf0f658d2ec4ff594170c6
chelo <77906668@institutourusayhua.edu.pe>
2026-09-07 11:35:00 -0500

    Add files via upload

---
+++ hola mundo2.py
@@ -0,0 +1,26 @@
+#Cómo asignar un nuevo valor a una variable ya existente
+var = 1
+print("variable con un valor primario: ", var)
+var = var + 1
+print("variable con un nuevo valor asignado: ", var)
+var = 100
+var = 200 + 300
+print(var)
+juan_david= 3
+madison = 5
+alex = 6
+print("Juan david tiene:",juan_david,"manzanas -->","Madison tiene:",madison,"manzanas -->", "alex tiene:",alex,"manzanas")
+total_manzanas = juan_david + madison + alex
+print("El total de manzanas es: ", total_manzanas)
+#Operadores Abreviados
+x = 3
+# X = X * 2
+x *= 2
+print("el resultado de x es: ", x)
+
+
+
+
+
+
+
